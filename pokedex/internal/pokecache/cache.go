1b699ad639434fa39ebb3c094eee6e4ff6ebc0ec
Jesus Gonzalez <reychucho859@gmail.com>
2026-02-07 21:02:58 -0500

    Mutex -> RWMutex for better performence

--- pokedex/internal/pokecache/cache.go
+++ pokedex/internal/pokecache/cache.go
@@ -7,7 +7,7 @@ import (
 
 type Cache struct {
 	cache map[string]cacheEntry
-	mu    *sync.Mutex
+	mu    *sync.RWMutex
 }
 
 type cacheEntry struct {
@@ -18,7 +18,7 @@ type cacheEntry struct {
 func NewCache(interval time.Duration) Cache {
 	c := Cache{
 		cache: make(map[string]cacheEntry),
-		mu:    &sync.Mutex{},
+		mu:    &sync.RWMutex{},
 	}
 
 	go c.reapLoop(interval)
@@ -36,8 +36,8 @@ func (c *Cache) Add(key string, val []byte) {
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
+	c.mu.RLock()
+	defer c.mu.RUnlock()
 	entry, found := c.cache[key]
 	return entry.val, found
 }
